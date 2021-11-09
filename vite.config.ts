d01737d2829240c8a7f6241b89b869489f6bf28b
Andrei Grini <Andrei.Grini@avanssoma.no>
2021-11-09 22:29:06 +0200

    vite plugins connected

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import svgr from 'vite-plugin-svgr'
+import { ViteAliases } from 'vite-aliases'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+    server: {
+        port: 9999,
+    },
+    plugins: [react(), svgr(), ViteAliases()],
 })
