daf79b40bd9f6a45a0b91f1fdda12276b36b06d4
Andrei Grini <Andrei.Grini@avanssoma.no>
2022-01-03 22:47:17 +0200

    fixed topNavMenu setup location.path in LocalStorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,32 @@
+import { useEffect } from 'react'
+import { useNavigate, useLocation } from 'react-router-dom'
 import { TopNavigationMenu } from './layout/TopNavigationMenu'
 import { AppRoutes } from './Routes'
 
 export const App = () => {
+    console.log('App rerendering')
     return (
         <>
+            <LocalStorageListener />
             <TopNavigationMenu />
             <AppRoutes />
         </>
     )
+}
+
+const LocalStorageListener = () => {
+    const navigate = useNavigate()
+    const { pathname } = useLocation()
+    useEffect(() => {
+        const savedLocation: string = JSON.parse(
+            localStorage.getItem('useLocation') || '',
+        )
+        savedLocation && navigate(savedLocation)
+    }, [])
+
+    useEffect(() => {
+        localStorage.setItem('useLocation', JSON.stringify(pathname))
+    }, [pathname])
+
+    return null
 }
--- src/layout/AnchorMenu.tsx
+++ src/layout/AnchorMenu.tsx
@@ -3,11 +3,10 @@ import styles from './SideMenu.module.scss'
 
 export const AnchorMenu: React.FC<{ anchors: IAnchor[] }> = ({ anchors }) => {
     return (
-        <ul className="absolute w-60 flex flex-col px-4 bg-red-500 right-0">
-            {/* <ul className={styles['anchors-list']}> */}
+        <ul className={styles['anchors-list']}>
             {anchors.map(({ id, title }) => (
-                <a href={id}>
-                    <li key={id}>{title}</li>
+                <a key={id} href={id}>
+                    <li>{title}</li>
                 </a>
             ))}
         </ul>
