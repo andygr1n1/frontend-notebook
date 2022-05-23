bf09f7281bfdb424b98bc4c6073cbae2279bf5a5
Grini Andrei <Andrei.Grini@avanssoma.no>
2022-05-23 21:22:38 +0300

    started mobile first

--- src/layout/Content.tsx
+++ src/layout/Content.tsx
@@ -2,7 +2,7 @@ import type { ReactNode } from 'react'
 
 export const Content: React.FC<{ children?: ReactNode }> = ({ children }) => {
     return (
-        <div className="flex flex-col overflow-auto px-28 w-[1000px] border-r border-l border-primary-color border-opacity-10 text-lg">
+        <div className="flex flex-col overflow-auto border-r border-l border-primary-color border-opacity-10 px-28 text-lg 2xl:w-[1000px]">
             {children}
         </div>
     )
