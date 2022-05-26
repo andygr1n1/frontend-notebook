73c73f1f2e39e963a46e67db63a780aaa3f3d41c
Grini Andrei <Andrei.Grini@avanssoma.no>
2022-05-27 00:27:02 +0300

    search hot button

--- src/layout/Content.tsx
+++ src/layout/Content.tsx
@@ -1,12 +1,17 @@
 import { observer } from 'mobx-react-lite'
 import type { ReactNode } from 'react'
-import { NavLink } from 'react-router-dom'
+import { NavLink, useLocation } from 'react-router-dom'
 import { useRootStore } from '../StoreProvider'
 import { ThemeMode } from './control-panel/color-theme/ThemeMode'
 import { PenColor } from './control-panel/pen-color/PenColor'
+import MenuRoundedIcon from '@mui/icons-material/MenuRounded'
+import ManageSearchRoundedIcon from '@mui/icons-material/ManageSearchRounded'
 
 export const Content: React.FC<{ children?: ReactNode }> = observer(({ children }) => {
     const { navigation_menu_mobile_status } = useRootStore()
+    const location = useLocation()
+    const isCurrentPathSearchPage = location.pathname === '/'
+
     return (
         <>
             <div className="flex h-full w-full flex-col overflow-auto border-r border-l border-primary-color border-opacity-10  text-lg 2xl:w-[1000px]">
@@ -14,10 +19,24 @@ export const Content: React.FC<{ children?: ReactNode }> = observer(({ children 
                     <MobileControlPanelModal />
                 ) : (
                     <div className="flex h-full w-full flex-col">
-                        <div className="flex flex-auto flex-col px-10">{children}</div>
+                        <div className="flex flex-auto flex-col px-14 2xl:px-10">
+                            {children}
+                        </div>
                         <MobileControlPanel />
                     </div>
                 )}
+                <div className="absolute bottom-16 left-3 flex flex-col gap-2 ">
+                    {!isCurrentPathSearchPage && (
+                        <NavLink
+                            to="/"
+                            className="rounded-full bg-primary-color p-1 !text-[14px] !leading-none !text-white">
+                            <ManageSearchRoundedIcon />
+                        </NavLink>
+                    )}
+                    <div className="bg-primary-color p-1 !text-[14px] !leading-none text-white 2xl:hidden">
+                        <MenuRoundedIcon />
+                    </div>
+                </div>
             </div>
         </>
     )
@@ -29,10 +48,9 @@ export const MobileControlPanel = observer(() => {
     const open = () => onChangeField('navigation_menu_mobile_status', true)
 
     return (
-        <div
-            onClick={open}
-            className="sticky bottom-0 flex h-12 w-full border-t border-t-primary-color bg-bg-color p-2 text-text-color 2xl:hidden">
+        <div className="sticky bottom-0 flex h-12 w-full border-t border-t-primary-color bg-bg-color p-2 text-text-color 2xl:hidden">
             <span
+                onClick={open}
                 className="
                         flex w-full cursor-pointer select-none items-center
                         justify-center border border-primary-color
